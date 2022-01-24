3f0ccbc36074c259967e9ec3df2505b0b586ac7c
sanjipun <iamsanjipun@gmail.com>
2022-01-24 21:22:18 +0545

    update index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Seo from '../components/SEO/Seo';
 
 let window = global.window;
 const Home: NextPage = () => {
-  console.log(window?.innerHeight);
   return (
     <div>
       <Seo title="Gokyo NFT | Lake of NFT's" subtitle="Lake of NFT's"></Seo>
