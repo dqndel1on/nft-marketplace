a77765a7f7153999460fd2d5c024bba0119a6d4b
sanjipun <iamsanjipun@gmail.com>
2022-01-24 21:22:03 +0545

    nft market place

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { link } from 'fs';
 
+const LINKS: { title: string; link: string }[] = [
+  { title: 'Home', link: '/' },
+  { title: 'Sell Digital Assets', link: 'create-item' },
+  { title: 'My Digital Assets', link: 'my-assets' },
+  { title: 'Creator Dashboard', link: 'creator-dashboard' },
+];
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto px-10">
+      <nav className="border-b p-6">
+        <p>Gokyo NFT's</p>
+        <div className="flex justify-between mt-10">
+          {LINKS.map((link, i: number) => (
+            <Link key={i} href={link.link}>
+              <a className="text-pink-600">{link.title}</a>
+            </Link>
+          ))}
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next';
 import Seo from '../components/SEO/Seo';
 
+let window = global.window;
 const Home: NextPage = () => {
+  console.log(window?.innerHeight);
   return (
     <div>
       <Seo title="Gokyo NFT | Lake of NFT's" subtitle="Lake of NFT's"></Seo>
-      <main className="text-red-500">Hello World</main>
+      <main className="text-red-500" style={{ minHeight: window?.innerHeight }}>
+        Hello World
+      </main>
       <footer></footer>
     </div>
   );
